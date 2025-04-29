63f53c651339b92145b292276b9e928a334617be
cedarLongballa <longbacedar@gmail.com>
2025-04-29 10:50:23 -0500

    moved things from appcomponent to the new homeComponent

--- Campus-Community-Forum/src/app/app.component.ts
+++ Campus-Community-Forum/src/app/app.component.ts
@@ -1,45 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { Database, ref, set, get, child } from '@angular/fire/database';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Campus Community Forum';
-  dataFromDB: any;
-
-  constructor(private db: Database) {}
-
-  writeData() {
-    const dbRef = ref(this.db, 'messages/1');
-    set(dbRef, {
-      username: "Caden",
-      message: "Hello, Firebase!"
-    }).then(() => {
-      console.log("Data written successfully!");
-    }).catch((error) => {
-      console.error("Error writing data: ", error);
-    });
-  }
-
-  readData() {
-    const dbRef = ref(this.db);
-    get(child(dbRef, `messages/1`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        this.dataFromDB = snapshot.val();
-        console.log(this.dataFromDB);
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }
 }
---
+++ Campus-Community-Forum/src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Database, ref, set, get, child } from '@angular/fire/database';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  title = 'Campus Community Forum';
+  dataFromDB: any;
+
+  constructor(private db: Database) {}
+
+  writeData() {
+    const dbRef = ref(this.db, 'messages/1');
+    set(dbRef, {
+      username: "Caden",
+      message: "Hello, Firebase!"
+    }).then(() => {
+      console.log("Data written successfully!");
+    }).catch((error) => {
+      console.error("Error writing data: ", error);
+    });
+  }
+
+  readData() {
+    const dbRef = ref(this.db);
+    get(child(dbRef, `messages/1`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        this.dataFromDB = snapshot.val();
+        console.log(this.dataFromDB);
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+}
