5761dbfc7bffc1f81f0cd6d053bc3553477ec443
cedarLongballa <longbacedar@gmail.com>
2025-04-29 10:18:54 -0500

    created other needed components

---
+++ Campus-Community-Forum/src/app/weather-bar/weather-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-bar',
+  imports: [],
+  templateUrl: './weather-bar.component.html',
+  styleUrl: './weather-bar.component.css'
+})
+export class WeatherBarComponent {
+
+}
