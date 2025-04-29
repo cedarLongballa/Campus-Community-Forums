24575bbbdb227c9f210b19f53ffdea3c29f6aaa1
caden-larson <cadenlarson7@gmail.com>
2025-04-29 09:54:10 -0500

    commit so i can switch to main branch

--- Campus-Community-Forum/src/app/app.component.ts
+++ Campus-Community-Forum/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,        // <-- 🔥 THIS IS MISSING IN YOURS
+  standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
