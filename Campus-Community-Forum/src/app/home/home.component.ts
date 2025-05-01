1830a49e8cf313c36a01ced65ffabf3bc6d99e55
cedarLongballa <longbacedar@gmail.com>
2025-05-01 10:06:41 -0500

    Adding Caden's changes

--- Campus-Community-Forum/src/app/home/home.component.ts
+++ Campus-Community-Forum/src/app/home/home.component.ts
@@ -17,31 +17,7 @@ export class HomeComponent {
 
   newsletterContent = this.dataService.newsletter;
 
-  constructor(private db: Database) {}
+  constructor() {}
 
-  writeData() {
-    const dbRef = ref(this.db, 'messages/1');
-    set(dbRef, {
-      username: "Caden",
-      message: "Hello, Firebase!"
-    }).then(() => {
-      console.log("Data written successfully!");
-    }).catch((error) => {
-      console.error("Error writing data: ", error);
-    });
-  }
-
-  readData() {
-    const dbRef = ref(this.db);
-    get(child(dbRef, `messages/1`)).then((snapshot) => {
-      if (snapshot.exists()) {
-        this.dataFromDB = snapshot.val();
-        console.log(this.dataFromDB);
-      } else {
-        console.log("No data available");
-      }
-    }).catch((error) => {
-      console.error(error);
-    });
-  }
+  
 }
